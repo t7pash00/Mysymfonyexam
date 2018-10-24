9c4d553c4afa9aa789b47ac0a2e99c7956f67adc
t7pash00 <t7pash00@students.oamk.fi>
2018-10-24 10:24:18 +0300

    changes made to controller and

---
+++ symfonyApp/src/Controller/TodoController.php
@@ -0,0 +1,15 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class TodoController extends AbstractController
+{
+    public function index()
+    {
+       $ 
+        ]);
+    }
+}
